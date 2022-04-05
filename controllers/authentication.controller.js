cddac217d58b2f6772df31e14e91025545acb085
aramisramirez <aramisramirez666@gmail.com>
2022-04-05 12:15:18 -0500

    Mejoras Login backend

---
+++ controllers/authentication.controller.js
@@ -0,0 +1,63 @@
+const bcrypt = require('bcrypt')
+require('dotenv').config()
+// const jwt = require('jsonwebtoken')
+const Guest = require('../models/guest.model')
+
+module.exports = class AuthenticationController {
+  async login (req, res) {
+    const { email, password } = req.body
+
+    // Buscar huésped por email (el email es único)
+    const guest = await Guest.findOne({
+      where: {
+        email: email
+      }
+    })
+
+    // const obtenerListado = await Guest.findOne(email)
+
+    // if (guest) {
+    //   window.localStorage.setItem('persona', guest)
+    // }
+
+    if (guest) { return res.status(200).send({ message: 'Accediendo' }) }
+
+    if (!guest) { return res.status(400).send({ message: 'Usuario y/o contraseña incorrectos' }) }
+
+    const match = await bcrypt.compare(password, guest.password)
+
+    if (match) { return res.status(200).send({ message: 'Accediendo' }) }
+
+    if (!match) { return res.status(400).send({ message: 'Usuario y/o contraseña incorrectos' }) }
+
+    // const token = jwt.sign({
+    //   guest_id: guest.guest_id
+    // }, process.env.JWT_SECRET, {
+    //   expiresIn: '24h'
+    // })
+
+    // res.cookie('authtoken', token, { secure: true, expiresIn: 24 * 60 * 60 })
+    // res.status(200).send({ token })
+  }
+
+  // async validate (req, res, next) {
+  //   const authorization = req.headers.authorization
+
+  //   if (!authorization) return res.status(401).send({ message: 'No autorizado' })
+
+  //   const [, token] = authorization.split('Bearer ')
+
+  //   try {
+  //     const valid = jwt.verify(token, process.env.JWT_SECRET)
+  //     if (valid) {
+  //       next()
+  //     } else {
+  //       res.status(401).send({ message: 'No autorizado' })
+  //     }
+  //   } catch (error) {
+  //     if (error.name === 'TokenExpiredError') {
+  //       res.status(401).send({ message: 'No autorizado' })
+  //     }
+  //   }
+  // }
+}
